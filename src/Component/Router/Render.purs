e31c3a5141300d6fd1910f5eb4841d6af7d777c5
Kévin Francart <kevin@danim.com>
2025-09-16 20:48:56 +0200

    misc

--- src/Component/Router/Render.purs
+++ src/Component/Router/Render.purs
@@ -2,7 +2,9 @@ module Component.Router.Render where
 
 import Prelude hiding (div)
 
-import CSS (backgroundColor, borderRadius, display, flex, flexDirection, flexGrow, fromInt, marginLeft, minHeight, pct, rem, row, vh, width)
+import CSS (backgroundColor, borderRadius, display, flex, flexDirection, fromInt, margin, minHeight, rem, row, vh)
+import CSS.Overflow (overflow)
+import CSS.Overflow as Overflow
 import Capability.Log (class Log)
 import Capability.Navigate (class Navigate)
 import Component.Page.About.Component as PageAboutComponent
@@ -12,10 +14,10 @@ import Component.Router.Route (Route(..))
 import Component.Router.Type (Action, State, Slots)
 import Effect.Aff.Class (class MonadAff)
 import Halogen as H
-import Halogen.HTML (div, div_, slot, text)
+import Halogen.HTML (div, slot, text)
 import Halogen.HTML.CSS (style)
 import Type.Proxy (Proxy(..))
-import Utils.Style (margin, padding)
+import Utils.Style (padding)
 
 render :: forall m. MonadAff m => Navigate m => Log m => State -> H.ComponentHTML Action Slots m
 render { route } =
@@ -28,25 +30,15 @@ render { route } =
     [ slot (Proxy :: Proxy "menu") unit MenuComponent.component unit absurd
     , div
         [ style do
-            marginLeft (rem MenuStyle.foldWidth)
-            flexGrow 1.0
+            margin (rem 2.0) (rem 2.0) (rem 2.0) (rem $ 2.0 + MenuStyle.foldWidth)
             display flex
+            backgroundColor (fromInt 0xffffff)
+            padding 1.0
+            borderRadius (rem 0.6) (rem 0.6) (rem 0.6) (rem 0.6)
+            overflow Overflow.hidden
         ]
-        [ div
-            [ style do
-                backgroundColor (fromInt 0xffffff)
-                padding 1.0
-                width (pct 100.0)
-                margin 2.0
-                borderRadius (rem 0.6) (rem 0.6) (rem 0.6) (rem 0.6)
-            ]
-            [ div []
-                [ div_
-                    [ case route of
-                        Home -> div [] [ text "🏠 Page d'accueil" ]
-                        About -> slot (Proxy :: Proxy "about") unit PageAboutComponent.component unit absurd
-                    ]
-                ]
-            ]
+        [ case route of
+            Home -> div [] [ text "🏠 Page d'accueil" ]
+            About -> slot (Proxy :: Proxy "about") unit PageAboutComponent.component unit absurd
         ]
     ]
