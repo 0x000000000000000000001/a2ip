bf9aaa3da65b800a8fce9c65bfe44d73086cc638
Kévin Francart <kevin@danim.com>
2025-09-19 14:33:29 +0200

    misc

--- src/Component/Page/About/Render.purs
+++ src/Component/Page/About/Render.purs
@@ -12,7 +12,7 @@ import Component.Page.About.Style.Card.Line as CardLine
 import Component.Page.About.Style.Card.Names as CardNames
 import Component.Page.About.Style.Card.Portrait as CardPortrait
 import Component.Page.About.Style.Sheet (sheet)
-import Component.Page.About.Type (Action, Slots, State, Member)
+import Component.Page.About.Type (Action, Member, Slots, State, email, job, phone, role)
 import Data.Maybe (Maybe, fromMaybe, maybe)
 import Data.String (Pattern(..), Replacement(..), replace)
 import Effect.Aff.Class (class MonadAff)
@@ -86,7 +86,7 @@ renderMemberCard member =
       ]
 
   lines =
-    line _.role "role"
-      <> line _.job "job"
-      <> line _.phone "phone"
-      <> line _.email "email"
+    line _.role role
+      <> line _.job job
+      <> line _.phone phone
+      <> line _.email email
