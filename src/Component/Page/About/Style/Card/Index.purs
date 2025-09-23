1392f9434a4d6437dd457352e4cce0c7ef18554e
0x1 <keywinf@gmail.com>
2025-09-23 17:26:56 +0200

    misc

--- src/Component/Page/About/Style/Card/Index.purs
+++ src/Component/Page/About/Style/Card/Index.purs
@@ -1,19 +1,18 @@
-module Component.Page.About.Style.Card.Sheet
-  ( sheet
+module Component.Page.About.Style.Card.Index
+  ( style
   ) where
 
 import Prelude
 
-import Halogen.HTML (HTML)
-import Halogen.HTML.CSS (stylesheet)
-import Component.Page.About.Style.Card.Card (style)
-import Component.Page.About.Style.Card.Portrait as Portrait
+import CSS as CSS
+import Component.Page.About.Style.Card.Card as Card
+import Component.Page.About.Style.Card.Line as Line
 import Component.Page.About.Style.Card.Names as Names
-import Component.Page.About.Style.Card.Line as Line 
+import Component.Page.About.Style.Card.Portrait as Portrait
 
-sheet :: forall p i. HTML p i
-sheet = stylesheet do
-  style   
+style :: CSS.CSS
+style = do
+  Card.style
   Portrait.style
-  Names.style 
+  Names.style
   Line.style
--- src/Component/Page/About/Style/Sheet.purs
+++ src/Component/Page/About/Style/Sheet.purs
@@ -7,15 +7,9 @@ import Prelude
 import Component.Page.About.Style.About (style)
 import Halogen.HTML (HTML)
 import Halogen.HTML.CSS (stylesheet)
-import Component.Page.About.Style.Card.Card as Card
-import Component.Page.About.Style.Card.Portrait as CardPortrait
-import Component.Page.About.Style.Card.Names as CardNames
-import Component.Page.About.Style.Card.Line as CardLine 
+import Component.Page.About.Style.Card.Index as Card 
 
 sheet :: forall p i. HTML p i
 sheet = stylesheet do
   style  
-  Card.style
-  CardPortrait.style
-  CardNames.style
-  CardLine.style
+  Card.style 
