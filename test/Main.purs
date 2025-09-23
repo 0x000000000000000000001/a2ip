5dad196b8324a02c3f0db7c6244b58a47d1b7807
0x1 <keywinf@gmail.com>
2025-09-23 17:37:42 +0200

    misc

--- test/Main.purs
+++ test/Main.purs
@@ -5,7 +5,7 @@ import Prelude
 import Effect (Effect)
 import Test.Spec.Reporter.Spec (specReporter)
 import Test.Spec.Runner.Node (runSpecAndExitProcess)
-import Test.Utils.Index as Utils 
+import Test.Utils.Index as Utils  
 
 main :: Effect Unit
 main = runSpecAndExitProcess [specReporter] do
