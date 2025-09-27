2a1f5d0aa5114a5790863866f24242a14455ec43
0x1 <keywinf@gmail.com>
2025-09-27 19:25:25 +0200

    misc

--- test/Utils/Html/Clean/Index.purs
+++ test/Utils/Html/Clean/Index.purs
@@ -4,6 +4,7 @@ import Prelude
 
 import Test.Spec (Spec)
 import Test.Utils.Html.Clean.CleanAttributesInTag as CleanAttributesInTag
+import Test.Utils.Html.Clean.CleanAttributesInTags as CleanAttributesInTags
 import Test.Utils.Html.Clean.FindUnescapedQuote as FindUnescapedQuote
 import Test.Utils.Html.Clean.RemoveAttribute as RemoveAttribute
 import Test.Utils.Html.Clean.RemoveDataAttributes as RemoveDataAttributes
@@ -11,6 +12,7 @@ import Test.Utils.Html.Clean.RemoveDataAttributes as RemoveDataAttributes
 spec :: Spec Unit
 spec = do
   CleanAttributesInTag.spec
+  CleanAttributesInTags.spec
   FindUnescapedQuote.spec
   RemoveAttribute.spec
   RemoveDataAttributes.spec
