ee9b3da5a86d48a54c8420a9d411564c1c39d2c5
0x1 <keywinf@gmail.com>
2025-09-29 13:26:29 +0200

    misc

--- src/Component/Page/About/HandleAction.purs
+++ src/Component/Page/About/HandleAction.purs
@@ -7,33 +7,32 @@ import Prelude
 
 import Capability.Log (class Log, Level(..), log)
 import Component.Page.About.Type (Action(..), State)
-import Data.Maybe (Maybe(..))
 import Effect.Aff.Class (class MonadAff)
 import Halogen as H
 
-membersTabId :: String
-membersTabId = "0"
+-- membersTabId :: String
+-- membersTabId = "0"
 
-membersTabName :: String
-membersTabName = "Membres A2IP"
+-- membersTabName :: String
+-- membersTabName = "Membres A2IP"
 
-commiteeTabId :: String
-commiteeTabId = "2092489064"
+-- commiteeTabId :: String
+-- commiteeTabId = "2092489064"
 
-commiteeTabName :: String
-commiteeTabName = "Comité international"
+-- commiteeTabName :: String
+-- commiteeTabName = "Comité international"
 
-googleSheetUrl :: String
-googleSheetUrl = "https://docs.google.com/spreadsheets/d/1k5wU7ARnjasX6y29AEDcpW06Zk_13I2XI6kwgKlsVhE"
+-- googleSheetUrl :: String
+-- googleSheetUrl = "https://docs.google.com/spreadsheets/d/1k5wU7ARnjasX6y29AEDcpW06Zk_13I2XI6kwgKlsVhE"
 
-googleSheetHtmlZipDownloadUrl :: String
-googleSheetHtmlZipDownloadUrl = googleSheetUrl <> "/export?format=zip"
+-- googleSheetHtmlZipDownloadUrl :: String
+-- googleSheetHtmlZipDownloadUrl = googleSheetUrl <> "/export?format=zip"
 
-tabIdToName :: String -> Maybe String
-tabIdToName tabId 
-  | tabId == membersTabId = Just membersTabName
-  | tabId == commiteeTabId = Just commiteeTabName
-  | otherwise = Nothing
+-- tabIdToName :: String -> Maybe String
+-- tabIdToName tabId 
+--   | tabId == membersTabId = Just membersTabName
+--   | tabId == commiteeTabId = Just commiteeTabName
+--   | otherwise = Nothing
 
 loadData :: forall o m. MonadAff m => Log m => H.HalogenM State Action () o m Unit
 loadData = do
@@ -43,14 +42,14 @@ loadData = do
   -- case result of
   --   Left err -> log Error $ "Failed to fetch ZIP: " <> show err
   --   Right response -> do
-        -- let tabName = fromMaybe "" $ tabIdToName tabId
---   htmlContent <- H.liftAff $ unzipGoogleSheetAndExtractHtml tabName response.body
+  --     let tabName = fromMaybe "" $ tabIdToName tabId
+  --     htmlContent <- H.liftAff $ unzipGoogleSheetAndExtractHtml tabName response.body
   --     members_ <- parseHtml htmlContent
   --     H.modify_ _ { members = members_ }
 
 handleAction :: forall o m. MonadAff m => Log m => Action -> H.HalogenM State Action () o m Unit
 handleAction = case _ of
-  LoadData -> loadData 
+  LoadData -> loadData
 
-extractMappingKeysAndValuesFromTable :: String -> { keys :: Array String , values :: Array (Array String) }
-extractMappingKeysAndValuesFromTable tableHtml = { keys: [], values: [] }
+-- extractMappingKeysAndValuesFromTable :: String -> { keys :: Array String , values :: Array (Array String) }
+-- extractMappingKeysAndValuesFromTable tableHtml = { keys: [], values: [] }
