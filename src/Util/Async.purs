032f94e3d98ac51cd73933d31285c1e51c77e924
0x1 <keywinf@gmail.com>
2025-10-04 19:27:25 +0200

    misc

--- src/Util/Async.purs
+++ src/Util/Async.purs
@@ -7,27 +7,21 @@ import Prelude
 import Concurrent.BoundedQueue (BoundedQueue)
 import Concurrent.BoundedQueue as BQ
 import Control.Parallel (parTraverse)
-import Data.Array ((!!), (..))
-import Data.Maybe (fromJust)
-import Data.Traversable (class Traversable, traverse, traverse_)
-import Effect (Effect)
+import Data.Array ((..))
+import Data.Traversable (class Traversable, traverse_)
 import Effect.Aff (Aff, bracket)
-import Effect.Aff.AVar as AVar
-import Effect.Class (liftEffect)
-import Effect.Ref as Ref
-import Partial.Unsafe (unsafePartial)
 
-sem :: Int -> Effect (BoundedQueue Unit)
+sem :: Int -> Aff (BoundedQueue Unit)
 sem n = do
   q <- BQ.new n
-  traverse_ (const $ BQ.write unit q) (1 .. n)
+  traverse_ (const $ BQ.write q unit) (1 .. n)
   pure q
 
 semAcq :: BoundedQueue Unit -> Aff Unit
-semAcq q = liftEffect (BQ.read q)
+semAcq q = BQ.read q
 
 semRel :: BoundedQueue Unit -> Aff Unit
-semRel q = liftEffect (BQ.write unit q)
+semRel q = BQ.write q unit
 
 parTraverseBounded
   :: forall f a b. Traversable f
@@ -36,7 +30,7 @@ parTraverseBounded
   -> f a          -- inputs
   -> Aff (f b)
 parTraverseBounded maxInFlight k xs = do
-  s <- liftEffect $ sem maxInFlight
+  s <- sem maxInFlight
   parTraverse
     (\x ->
       bracket
