7e33e136017ad162914dd97c635f0b7a488cad5e
0x1 <keywinf@gmail.com>
2025-10-05 20:12:23 +0200

    misc

--- test/Component/Page/About/HandleAction.purs
+++ test/Component/Page/About/HandleAction.purs
@@ -5,10 +5,10 @@ import Prelude
 import Test.Spec (Spec)
 import Test.Util.Describe (describe)
 
-import Test.Component.Page.About.HandleAction.ExtractMappingKeysAndValuesFromTable as ExtractMappingKeysAndValuesFromTable
+import Test.Component.Page.About.HandleAction.ExtractMappingKeysAndValuesFromTableHtml as ExtractMappingKeysAndValuesFromTableHtml
 import Test.Component.Page.About.HandleAction.ConvertExtractedDataToMembers as ConvertExtractedDataToMembers
 
 spec :: Spec Unit
 spec = describe do
-  ExtractMappingKeysAndValuesFromTable.spec
+  ExtractMappingKeysAndValuesFromTableHtml.spec
   ConvertExtractedDataToMembers.spec 
