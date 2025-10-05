9751e49729c517707befff2996b6ceb4b1136a25
0x1 <keywinf@gmail.com>
2025-10-05 10:51:21 +0200

    misc

--- src/Config.js
+++ src/Config.js
@@ -1,4 +1,8 @@
-export const _config = {
-  rootDir: import.meta.dirname.split('/output/')[0],
-  env: "${ENV:-dev}"
+export const _config = function () {
+
+  
+  return {
+    rootDir: import.meta.dirname.split('/output/')[0],
+    env: typeof process !== 'undefined' && process.env && process.env.ENV ? process.env.ENV : 'dev'
+  };
 };
