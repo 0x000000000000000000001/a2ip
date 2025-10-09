1684d69f61e5822932e7378998c17024c8779bee
0x1 <keywinf@gmail.com>
2025-10-09 13:55:37 +0200

    misc

--- src/Component/Common/PrettyErrorImage/HandleAction.purs
+++ src/Component/Common/PrettyErrorImage/HandleAction.purs
@@ -6,12 +6,9 @@ module Component.Common.PrettyErrorImage.HandleAction
 import Prelude
 
 import Capability.AppM (AppM)
-import Capability.Log (debug)
 import Component.Common.PrettyErrorImage.Type (State, Slots, Action(..), Output)
 import Halogen (HalogenM, modify_)
 
 handleAction :: Action -> HalogenM State Action Slots Output AppM Unit
 handleAction = case _ of  
-  HandleError -> do 
-    debug "abc"
-    modify_ _ { errored = true } 
+  HandleError -> modify_ _ { errored = true }
