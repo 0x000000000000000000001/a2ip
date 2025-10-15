04592842424cee60e29b1461428f730ea25769cd
Kévin Francart <kevin@danim.com>
2025-10-15 17:02:27 +0200

    misc

--- src/Component/Common/PrettyErrorImage/HandleAction.purs
+++ src/Component/Common/PrettyErrorImage/HandleAction.purs
@@ -11,9 +11,9 @@ import Halogen (HalogenM, modify_)
 handleAction :: Action -> HalogenM State Action Slots Output AppM Unit
 handleAction = case _ of
   HandleError -> modify_ $ \s -> s { errorCount = 1 + s.errorCount }
-  Receive input -> modify_ $ \s -> s
-    { src = input.src
-    , fallbackSrc = input.fallbackSrc
-    , errorCount = s.src /= input.src || s.fallbackSrc /= input.fallbackSrc ? 0 ↔ s.errorCount
-    , class_ = input.class_
+  Receive i -> modify_ $ \s -> s
+    { src = i.src
+    , fallbackSrc = i.fallbackSrc
+    , errorCount = s.src /= i.src || s.fallbackSrc /= i.fallbackSrc ? 0 ↔ s.errorCount
+    , class_ = i.class_
     }
