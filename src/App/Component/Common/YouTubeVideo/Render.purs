e2449d097a255b4afe05a8472806ab7b873a3694
Kévin Francart <kevin@danim.com>
2025-10-26 17:15:11 +0100

    misc

--- src/App/Component/Common/YouTubeVideo/Render.purs
+++ src/App/Component/Common/YouTubeVideo/Render.purs
@@ -4,10 +4,38 @@ module App.Component.Common.YouTubeVideo.Render
 
 import Proem hiding (div)
 
+import App.Component.Common.YouTubeVideo.Style.Sheet (sheet)
+import App.Component.Common.YouTubeVideo.Style.YouTubeVideo (classId)
 import App.Component.Common.YouTubeVideo.Type (Action, Slots, State)
 import App.Util.Capability.AppM (AppM)
+import Data.String (Pattern(..), Replacement(..), contains, replaceAll)
 import Halogen (ComponentHTML)
-import Halogen.HTML (text)
+import Halogen.HTML (div)
+import Html.Renderer.Halogen (render_)
+import Util.Style (class_)
 
 render :: State -> ComponentHTML Action Slots AppM
-render s = text $ "youtube video: " <> s.url
+render s = 
+  div 
+    [ class_ classId ]
+    [ sheet
+    , render_ $ iframe $ convertToEmbedUrl s.url 
+    ]
+
+iframe :: String -> String 
+iframe embedUrl = """
+<iframe 
+  src=" """ <> embedUrl <> """ " 
+  frameborder="0" 
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+  allowfullscreen>
+</iframe>
+"""
+
+convertToEmbedUrl :: String -> String
+convertToEmbedUrl url
+  | contains (Pattern "youtu.be/") url = 
+      replaceAll (Pattern "youtu.be/") (Replacement "youtube.com/embed/") url
+  | contains (Pattern "watch?v=") url = 
+      replaceAll (Pattern "watch?v=") (Replacement "embed/") url
+  | otherwise = url
--- src/App/Component/Common/YouTubeVideo/Style/YouTubeVideo.purs
+++ src/App/Component/Common/YouTubeVideo/Style/YouTubeVideo.purs
@@ -5,14 +5,35 @@ module App.Component.Common.YouTubeVideo.Style.YouTubeVideo
 
 import Proem hiding (top)
 
+import CSS (star)
 import CSS as CSS
-import Util.Style (displayFlex, justifyContentCenter, (.?))
+import Util.Style (displayFlex, heightPct100, heightRem, justifyContentCenter, widthPct100, widthRem, (.&¨), (.?), (.|*¨), (.|>), (.|>¨), (:?))
 
 classId :: String
 classId = "U7IRxxSNd"
+
+ratio :: Number 
+ratio = 16.0 / 9.0
+
+height :: Number
+height = 25.0
 
 style :: CSS.CSS
 style = do
   classId .? do
     displayFlex
     justifyContentCenter
+    widthRem $ ratio * height
+    heightRem height
+
+  __potentialIframeContainer :? do 
+    widthPct100
+    heightPct100
+
+  __iframe :? do 
+    widthPct100
+    heightPct100
+  
+  where 
+  __potentialIframeContainer = classId .|> star
+  __iframe = classId .|*¨ "iframe"
